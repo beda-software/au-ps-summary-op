47eee3440b8e685b6fa61cdb37a0e33c538f9554
Dmitry Shutov <dmitry.s@beda.software>
2025-03-06 13:14:35 +0700

    Add sort plugin for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
-import tseslint from 'typescript-eslint';
-import importPlugin from 'eslint-plugin-import';
 import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import sort from 'eslint-plugin-sort';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-  files: ['**/*.{ts,tsx}', '**/*.d.ts'],
-  extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
-});
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  sort.configs['flat/recommended'],
+  {
+    extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
+    files: ['**/*.{ts,tsx}', '**/*.d.ts'],
+  },
+);
